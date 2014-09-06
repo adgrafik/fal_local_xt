b6318aa4477c0101ca1a85ac1cf8917374164983
Arno Dudek <webmaster@adgrafik.at>
2014-09-06 16:52:41 +0200

    [TASK] Added documentation.

---
+++ ext_emconf.php
@@ -0,0 +1,56 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "fal_local_xt".
+ *
+ * Auto generated 24-08-2014 09:56
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'FAL Local XT Driver',
+	'description' => 'Provides an extended local driver for the TYPO3 File Abstraction Layer (FAL) with the possibility to use a different public URL.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.0.0',
+	'dependencies' => 'cms,version',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Arno Dudek',
+	'author_email' => 'webmaster@adgrafik.at',
+	'author_company' => 'ad:grafik',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'cms' => '',
+			'version' => '',
+			'php' => '5.3.3-0.0.0',
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'suggests' => 
+	array (
+	),
+);
+
+?>
